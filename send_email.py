454e82b572713c6eb7d1254084bc9b0cb3365664
inonpeled <inon.peled@gmail.com>
2018-12-19 12:25:45 +0100

    Initial commit

--- send_email.py
+++ send_email.py
@@ -1,23 +1,28 @@
+import json
 from email.message import EmailMessage
 import smtplib
 
-gmail_user = 'dtu.scripts.email@gmail.com'
-gmail_password = 'notaverysecurepassword123'
 
-sent_from = gmail_user
-to = ['inon.peled@gmail.com', 'inonpe@dtu.dk']
-subject = 'Subject'
-body = 'Hello'
+def get_smtp_credentials(config_path):
+    with open(config_path, 'r') as f:
+        return json.load(f)
 
-msg = EmailMessage()
-msg['Subject'] = 'Test message 2, ignore'
-msg['From'] = gmail_user
-msg['To'] = to[0]
-msg.set_content('Hello')
 
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.ehlo()
-server.login(gmail_user, gmail_password)
-server.send_message(msg)
-server.close()
-print('Email sent!')
+def send(subject, body, smtp_config_path='smtp_config.json'):
+    config = get_smtp_credentials(smtp_config_path)
+    msg = EmailMessage()
+    msg['From'] = config['user']
+    msg['To'] = config['to']
+    msg['Subject'] = subject
+    msg.set_content(body)
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(config['user'], config['password'])
+    server.send_message(msg)
+    server.close()
+    print('Email sent')
+
+
+if __name__ == '__main__':
+    send('Testing 123', 'Ignore this message')
